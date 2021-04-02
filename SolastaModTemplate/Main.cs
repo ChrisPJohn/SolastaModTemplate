f58341928506f0c306eee9408787cd67e4032f5d
ThyWoof <pauloesquilo@gmail.com>
2021-04-02 15:56:12 -0700

    first commit

---
+++ SolastaModTemplate/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+using SolastaModApi;
+
+namespace SolastaModTemplate
+{
+    public class Main
+    {
+        // [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+
+        public static void Error(Exception ex)
+        {
+            if (logger != null) logger.Error(ex.ToString());
+        }
+
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+
+                ModBeforeDBReady();
+
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        static class MainMenuScreen_RuntimeLoaded_Patch
+        {
+            static void Postfix()
+            {
+                ModAfterDBReady();
+            }
+        }
+
+        // ENTRY POINT IF YOU NEED SERVICE LOCATORS ACCESS
+        static void ModBeforeDBReady()
+        {
+            
+        }
+
+        // ENTRY POINT IF YOU NEED SAFE DATABASE ACCESS
+        static void ModAfterDBReady()
+        {
+            // Use DatabaseHelper Class to faciliate access to blueprints
+
+            var cleric = DatabaseHelper.CharacterClassDefinitions.Cleric;
+            var skeleton = DatabaseHelper.MonsterDefinitions.Skeleton;
+        }
+    }
+}
