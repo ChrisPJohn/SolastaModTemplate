99c8c4e9cb2a1ba0310f2312b355a33127e0dba2
ThyWoof <pauloesquilo@gmail.com>
2021-04-17 17:10:43 -0700

    cleanup

--- CREATE_NEW_MOD.ps1
+++ CREATE_NEW_MOD.ps1
@@ -1,7 +1,6 @@
 # get user input
 $TEMPLATE = "SolastaModTemplate"
 $MOD_NAME = Read-Host -Prompt "Enter your Mod/Repo name (no special characters): "
-$MOD_DESC = Read-Host -Prompt "Enter an one line Mod description: "
 $GIT_USER = Read-Host -Prompt "Enter your GitHub username: "
 
 # confirm destructive action
@@ -29,18 +28,13 @@ Rename-Item "$MOD_NAME\$TEMPLATE\$TEMPLATE.csproj" "$MOD_NAME.csproj"
 Rename-Item "$MOD_NAME\$TEMPLATE" "$MOD_NAME"
 Rename-Item "$MOD_NAME\$TEMPLATE.sln" "$MOD_NAME.sln"
 
-# create repo on GitHub
-# $CREDENTIAL = Get-Credential -UserName $GIT_USER -Message "Enter your GitHub credentials"
-# $BODY = @{name="$MOD_NAME"; description="$MOD_DESCRIPTION"}
-# Invoke-RestMethod -Uri "https://api.github.com/user/repos" -Method POST -Body $BODY
-while( -not ( ($choice= (Read-Host "Manually create your Repo <$MOD_NAME> on GitHub.com. CAN I CONTINUE?")) -match "y|n")){ "Y or N ?"}
-if($choice.ToUpper() -eq "N") {exit}
-
 # first commit
 cd "$MOD_NAME"
 git init
 git add .
 git commit -m "initial commit by SolastaMod"
+while( -not ( ($choice= (Read-Host "Have you created your repo *<$MOD_NAME>* on GitHub.com?")) -match "y|n")){ "Y or N ?"}
+if($choice.ToUpper() -eq "N") {exit}
 git remote add origin "https://github.com/$GIT_USER/$MOD_NAME.git"
 git push --set-upstream origin master
 cd ..
