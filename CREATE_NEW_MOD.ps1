68380438f7226b40f70fcbd2b9d33b9403273763
ThyWoof <pauloesquilo@gmail.com>
2021-04-17 19:41:24 -0700

    add git username to repo info.json

--- CREATE_NEW_MOD.ps1
+++ CREATE_NEW_MOD.ps1
@@ -1,11 +1,13 @@
 # get user input
 $TEMPLATE = "SolastaModTemplate"
-$MOD_NAME = Read-Host -Prompt "Enter your Mod/Repo name (no special characters): "
-$GIT_USER = Read-Host -Prompt "Enter your GitHub username: "
+$GIT_REPO = Read-Host -Prompt "Enter your GitHub repository name"
+$GIT_USER = Read-Host -Prompt "Enter your GitHub username"
 
 # confirm destructive action
-while( -not ( ($choice= (Read-Host "Existing $MOD_NAME folder will be recreated. ARE YOU SURE?")) -match "y|n")){ "Y or N ?"}
-if($choice.ToUpper() -eq "N") {exit}
+while( -not (($choice=(Read-Host "Existing $GIT_REPO folder will be deleted and recreated. ARE YOU SURE?")) -match "y|n"))
+    { "Y or N ?"}
+if($choice.ToUpper() -eq "N") 
+    {exit}
 
 # download and unzip Solasta Mod Template to New Mod Name
 Invoke-RestMethod `
@@ -13,28 +15,34 @@ Invoke-RestMethod `
     -OutFile "$TEMPLATE.zip"
 Remove-Item -Recurse -Force "$TEMPLATE-main" -ErrorAction Ignore
 Expand-Archive -Path "$TEMPLATE.Zip" -DestinationPath .
-Remove-Item -Recurse -Force "$MOD_NAME" -ErrorAction Ignore
-Rename-Item "$TEMPLATE-main" "$MOD_NAME"
+Remove-Item -Recurse -Force "$GIT_REPO" -ErrorAction Ignore
+Rename-Item "$TEMPLATE-main" "$GIT_REPO"
 
 # replace all SolastaModTemplate with ModName on all files
-$FILES = Get-ChildItem -Path ".\$MOD_NAME" -Recurse -File | select -ExpandProperty FullName
+$FILES = Get-ChildItem -Path ".\$GIT_REPO" -Recurse -File | select -ExpandProperty FullName
 foreach($FILE in $FILES)
 {
-    ((Get-Content -path $FILE -Raw) -replace $TEMPLATE, $MOD_NAME) | Set-Content -Path $FILE
+    (((Get-Content -path $FILE -Raw) `
+        -replace $TEMPLATE, $GIT_REPO) `
+        -replace "SolastaMods", $GIT_USER) | Set-Content -Path $FILE
 }
 
 # rename necessary files
-Rename-Item "$MOD_NAME\$TEMPLATE\$TEMPLATE.csproj" "$MOD_NAME.csproj"
-Rename-Item "$MOD_NAME\$TEMPLATE" "$MOD_NAME"
-Rename-Item "$MOD_NAME\$TEMPLATE.sln" "$MOD_NAME.sln"
+Rename-Item "$GIT_REPO\$TEMPLATE\$TEMPLATE.csproj" "$GIT_REPO.csproj"
+Rename-Item "$GIT_REPO\$TEMPLATE" "$GIT_REPO"
+Rename-Item "$GIT_REPO\$TEMPLATE.sln" "$GIT_REPO.sln"
+Remove-Item "$GIT_REPO\README.md"
+Rename-Item "$GIT_REPO\README-TEMPLATE.md" "README.md"
 
 # first commit
-cd "$MOD_NAME"
+cd "$GIT_REPO"
 git init
 git add .
 git commit -m "initial commit by SolastaMod"
-while( -not ( ($choice= (Read-Host "Have you created your repo *<$MOD_NAME>* on GitHub.com?")) -match "y|n")){ "Y or N ?"}
-if($choice.ToUpper() -eq "N") {exit}
-git remote add origin "https://github.com/$GIT_USER/$MOD_NAME.git"
+while( -not (($choice=(Read-Host "Have you created your repo <$GIT_REPO> on GitHub")) -match "y|n"))
+    { "Y or N ?"}
+if($choice.ToUpper() -eq "N")
+    {exit}
+git remote add origin "https://github.com/$GIT_USER/$GIT_REPO.git"
 git push --set-upstream origin master
 cd ..
