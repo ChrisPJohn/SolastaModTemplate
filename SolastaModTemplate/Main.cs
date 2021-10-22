5228d9ceb12f41c621e6baf6188e152bba51d47c
Paulo Monteiro <pauloesquilo@gmail.com>
2021-10-22 16:43:29 -0700

    cleanup

--- SolastaModTemplate/Main.cs
+++ SolastaModTemplate/Main.cs
@@ -18,7 +18,7 @@ namespace SolastaModTemplate
         internal static void Warning(string msg) => Logger?.Warning(msg);
         internal static UnityModManager.ModEntry.ModLogger Logger { get; private set; }
         internal static ModManager<Core, Settings> Mod;
-        internal static MenuManager Menu;
+        //internal static MenuManager Menu;
         internal static Settings Settings { get { return Mod.Settings; } }
 
         internal static bool Load(UnityModManager.ModEntry modEntry)
